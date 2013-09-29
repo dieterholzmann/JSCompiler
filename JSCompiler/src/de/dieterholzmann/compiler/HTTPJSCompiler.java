01a44277b9bedf20d0b5817afe0ffd482b621289
Dieter Holzmann <dimastij@gmx.net>
2013-09-29 20:41:12 +0200

    find js files and base url

--- JSCompiler/src/de/dieterholzmann/compiler/HTTPJSCompiler.java
+++ JSCompiler/src/de/dieterholzmann/compiler/HTTPJSCompiler.java
@@ -5,6 +5,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class HTTPJSCompiler {
 	public static void main(String[] args) {
@@ -19,34 +22,53 @@ public class HTTPJSCompiler {
 		// String externalJavascriptResources[] = {
 		// "http://dothot.de/script.js"
 		// };
+		ArrayList<String> files = new ArrayList<String>();
 		URL url = null;
 		try {
-			url = new URL("http://dothot.de/");
+			url = new URL(this.url);
 		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
 		BufferedReader in = null;
 		String ln;
 		String html = "";
+		
 		try {
 			in = new BufferedReader(new InputStreamReader(url.openStream()));
-			
+
 			while ((ln = in.readLine()) != null) {
 				html += ln;
+				if(ln.indexOf("base") > 0)
+					setBaseURL(ln);
 			}
 			in.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
+		Pattern p = Pattern.compile("src=\"(.*?)\"");
+		Matcher m = p.matcher(html);
 		
-		System.out.println(html);
 		
-		String pattern = "(?i)(<script.*?>)";
-		System.out.println(html.matches(".*(<script.*?>)"));
+
+		while (m.find()) {
+			if (m.group(1).indexOf(".js") > 0) {
+				files.add(m.group(1));
+			}
+		}
+	}
+	
+	private void setBaseURL(String str) {
+		Pattern p = Pattern.compile("<base href=\"(.*?)\"");
+		Matcher m = p.matcher(str);
+		
+		while (m.find()) {
+			if(!m.group(1).equals(""))
+				baseURL = m.group(1);
+		}
 	}
 
-//	private static URL url;
+	 private String url = "";
+	 private String baseURL = null;
 }
